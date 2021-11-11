32408bc63a5f2478ca6c2d9aa225971d81524039
HELIO MARCUS NERY FIDALGO <hmnff@hotmail.com>
2021-11-11 16:04:55 +0000

    Initial Commit

---
+++ MyNotes App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyNotes App
+//
+//  Created by Helio Marcus Nery Fidalgo on 11/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
